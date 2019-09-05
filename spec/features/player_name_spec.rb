05659cb1a90f6b761f89c022cee5906366bae4c7
Student <student@Makerss-MacBook-Pro-7.local>
2019-09-05 14:10:17 +0100

    refactor tests with helper method

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,18 +1,12 @@
 feature "names form" do
   scenario "expect player names" do
-    visit('/')
-    fill_in('Player1', with: 'James')
-    fill_in('Player2', with: 'Mike')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Player: James')
     expect(page).to have_content('Player: Mike')
   end
 
   scenario "expect player names" do
-    visit('/')
-    fill_in('Player1', with: 'James')
-    fill_in('Player2', with: 'Mike')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('James - hit points: 10')
   end
 end
