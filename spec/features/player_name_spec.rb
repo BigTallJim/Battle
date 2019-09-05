f95d8890391293aca3832ce132b5ad85864802ff
James Holton <james_holton@yahoo.co.uk>
2019-09-05 14:01:11 +0100

    Split into two feature tests

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -4,6 +4,15 @@ feature "names form" do
     fill_in('Player1', with: 'James')
     fill_in('Player2', with: 'Mike')
     click_button('Submit')
-    expect(page).to have_content('Player: James - hit points: 10 Player: Mike - hit points: 10')
+    expect(page).to have_content('Player: James')
+    expect(page).to have_content('Player: Mike')
+  end
+
+  scenario "expect player names" do
+    visit('/')
+    fill_in('Player1', with: 'James')
+    fill_in('Player2', with: 'Mike')
+    click_button('Submit')
+    expect(page).to have_content('James - hit points: 10')
   end
 end
